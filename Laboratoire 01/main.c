7fab30a3f03304be74d7743c2a2beac5d9573743
algorithm69 <duvalsayad@yahoo.fr>
2025-01-15 11:23:30 -0500

    Illustration du cours 01 supprimee. Ajout du repertoire pour le laboratoire 01.

---
+++ Laboratoire 01/main.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+
+// Pour ce premier laboratoire, on choisit une organisation par fonctions, dans un projet CLion d'un seul fichier.
+
+void exercice01() {
+    //Écrivez un programme qui affiche à l?écran la phrase latine « ab ovo usque ad
+    //mala ».
+    printf("Exercice 1 :\n");
+    printf("ab ovo usque ad mala\n");
+}
+
+void exercice02() {
+    //Le Watt est une unité de puissance qui correspond au transfert d?un joule
+    //pendant une seconde dans un système énergétique. Sachant qu?un Watt
+    //correspond à 0.2388459 calorie par seconde, écrivez un programme qui saisit
+    //un nombre de Watt au clavier et affiche la puissance équivalente exprimée en
+    //calorie par seconde.
+    printf("Exercice 2 :\n");
+    printf("A faire...\n");
+}
+
+void exercice03() {
+    //Soit le schéma électrique ci-dessous qui se compose d?une alimentation
+    //continue et d?une résistance.
+    //La tension se note V (exprimée en Volt) et la résistance se note R (exprimée
+    //en Ohm). Sachant que la loi d?Ohm affirme que l?intensité du courant I (en
+    //Ampère) peut être calculée à l?aide de la formule I = V / R, écrivez un
+    //programme qui saisit la tension et la résistance et qui affiche l?intensité.
+    printf("Exercice 3 :\n");
+    printf("A faire...\n");
+}
+
+void exercice04() {
+    //Si on laisse tomber un objet sphérique dans l?atmosphère, celui-ci accélère
+    //jusqu?à ce que son poids compense exactement la résistance de l?air. La
+    //formule qui suit permet de calculer la vitesse limite V d?un tel objet.
+    //où :
+    //- m = la masse de la sphère (en kg);
+    //- g = l?accélération de la pesanteur (en m/s2) que nous fixerons à 9.81;
+    //- ? = la masse volumique de l?air (en kg/m3) que nous fixerons à 1.2;
+    //- r = le rayon de la sphère (en m).
+    //Écrivez un programme qui saisit la masse ainsi que le rayon de la sphère et
+    //qui affiche sa vitesse maximale en chute libre.
+    printf("Exercice 4 :\n");
+    printf("A faire...\n");
+}
+
+void exercice05() {
+    //Écrivez un programme qui saisit au clavier deux entiers : un nombre
+    //d'étudiants et une taille d'équipe. Elle affiche à l'écran le nombre d'équipes de
+    //cette taille qui peuvent être formées et le nombre d'étudiants en surplus qui ne
+    //peuvent être placés dans une équipe.
+    printf("Exercice 5 :\n");
+    printf("A faire...\n");
+}
+
+void exercice06() {
+    //Avant le 15 février 1971, la monnaie britannique se composait des
+    //dénominations suivantes :
+    //- la livre (pound) qui correspondait à 20 shillings.
+    //- le shilling qui était subdivisé en 12 pennys.
+    //- le penny.
+    //Écrivez un programme qui saisit un nombre de pennys au clavier, convertit
+    //ceux-ci en un maximum de livres et de shillings et affiche à l'écran le nombre
+    //de livres et de shillings obtenus ainsi que le nombre de pennys restant.
+    printf("Exercice 6 :\n");
+    printf("A faire...\n");
+}
+
+void exercice07() {
+    //Écrivez un programme qui saisit deux nombres entiers au clavier et qui affiche
+    //le plus petit de ces deux nombres.
+    printf("Exercice 7 :\n");
+    printf("A faire...\n");
+}
+
+void exercice08() {
+    //Écrivez un programme qui saisit deux nombres entiers au clavier et qui affiche
+    //le plus grand de ces deux nombres.
+    printf("Exercice 8 :\n");
+    printf("A faire...\n");
+}
+
+void exercice09() {
+    //Le nombre de Reynolds sert à caractériser un écoulement en mécanique
+    //des fluides. Il se calcule de la façon suivante :
+    //V = vitesse caractéristique du fluide (en m/s);
+    //L = la dimension caractéristique (en m);
+    //v = la viscosité cinématique du fluide (en m2/s).
+    //Un écoulement est laminaire lorsque R est inférieur à 2000, turbulent lorsque
+    //R est supérieur à 4000 et transitoire dans tous les autres cas.
+    //Écrivez un programme qui saisit V, L ainsi que v et qui affiche si l?écoulement
+    //est laminaire, turbulent ou transitoire.
+    printf("Exercice 9 :\n");
+    printf("A faire...\n");
+}
+
+void exercice10() {
+    //On vous informe que sur un chantier on utilise la recette ci-dessous pour
+    //fabriquer 1 m3 de béton.
+    //Écrivez un programme qui saisit un nombre de kg de ciment, de sable et de
+    //gravier et qui affiche le volume maximal de béton qui peut alors être produit.
+    printf("Exercice 10 :\n");
+    printf("A faire...\n");
+}
+
+int main(void) {
+    exercice01();
+    exercice02();
+    exercice03();
+    exercice04();
+    exercice05();
+    exercice06();
+    exercice07();
+    exercice08();
+    exercice09();
+    exercice10();
+
+    return 0;
+}
