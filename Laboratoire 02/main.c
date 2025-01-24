ae62210be9524e470fab0942d75dfbe36fd36f0a
algorithm69 <duvalsayad@yahoo.fr>
2025-01-24 16:04:06 -0500

    premier commit pour ce laboratoire 2. Ne contient qu'un squelette de projet.

---
+++ Laboratoire 02/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+void question01() {
+
+}
+
+void question02() {
+
+}
+
+void question03() {
+
+}
+
+void question04() {
+
+}
+
+void question05() {
+
+}
+
+void question06() {
+
+}
+
+void question07() {
+
+}
+
+void question08() {
+
+}
+
+void question09() {
+
+}
+
+void question10() {
+
+}
+
+int main(void) {
+    question01();
+    question02();
+    question03();
+    question04();
+    question05();
+    question06();
+    question07();
+    question08();
+    question09();
+    question10();
+
+    return 0;
+}
